74426817be8ece0057dff5bd64147d83f2257057
Emre Tanrikulu <93709537+TanrikuluEmre@users.noreply.github.com>
2024-01-02 13:59:41 +0300

    Add files via upload

---
+++ BitirmeProjesi.py
@@ -0,0 +1,48 @@
+import os
+import pyedflib
+import numpy as np
+import scipy.signal
+import matplotlib.pyplot as plt
+
+eeg_directory = 'EEG'
+spectrogram_directory = 'spectrograms'
+os.makedirs(spectrogram_directory, exist_ok=True)
+
+eeg_files = [file for file in os.listdir(eeg_directory) if file.endswith('.edf')]
+
+for eeg_file in eeg_files:
+    try:
+        edf_file = pyedflib.EdfReader(os.path.join(eeg_directory, eeg_file))
+        num_channels = edf_file.signals_in_file
+
+        for channel in range(num_channels):
+            eeg_data = edf_file.readSignal(channel)
+
+            # 4-30 Hz aralığında bir band-pass filtre oluşturun
+            fs = edf_file.getSampleFrequency(channel)
+            nyquist = 0.5 * fs
+            low = 4 / nyquist
+            high = 30 / nyquist
+            b, a = scipy.signal.butter(4, [low, high], btype='band')
+
+            # Filtreleme işlemini uygulayın
+            filtered_data = scipy.signal.lfilter(b, a, eeg_data)
+
+            # Spektrogram oluşturun
+            plt.specgram(filtered_data, Fs=fs, NFFT=256, noverlap=128, cmap='viridis')
+
+            # Spektrogramu görselleştirin ve kaydedin
+            plt.xlabel('Zaman (s)')
+            plt.ylabel('Frekans (Hz)')
+            plt.title(f'EEG Spektrogram - Dosya: {eeg_file} - Kanal {channel + 1}')
+            plt.colorbar(label='Güç (dB)')
+
+            save_path = os.path.join(spectrogram_directory, f'{os.path.splitext(eeg_file)[0]}_channel{channel + 1}_spektrogram.png')
+            plt.savefig(save_path)
+            plt.close()
+
+        edf_file.close()
+
+    except Exception as e:
+        print(f"Hata oluştu: {e}")
+        continue
